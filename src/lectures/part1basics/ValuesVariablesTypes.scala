3f18f8c63a90eeffd50ed131182b13ec43f9d4cb
Matias Galleguillos <matotias@gmail.com>
2019-07-25 19:02:45 -0400

    add part1 class code

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,26 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+
+  // Val
+  // They are immutable
+
+  val aString: String = "hello"
+  val anotherString: String = "goodbye"
+
+  val aBoolean: Boolean = true
+  val aChar: Char = 'm'
+  val anInt: Int = 3
+  val aShort: Short = 1989
+  val aLong: Long = 3128301244324214128L
+  val aFloat: Float = 0.3f
+  var aDouble: Double = 1.61
+
+  // variables
+  var aVariable: int = 3
+  aVariable = 2
+}
